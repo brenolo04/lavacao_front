3706ee70faa0ead154ab595ac84c36cd405f6d7e
brenolo04 <brenocaires04@gmail.com>
2025-05-16 09:05:53 -0300

    some stuff

---
+++ src/layout/navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import {Navbar as FBNavBar, NavbarBrand} from "flowbite-react";
+
+function Navbar() {
+    return (
+        <>
+            <FBNavBar fluid rounded>
+            </FBNavBar>
+        </>
+    )
+} 
+
+export default Navbar;
